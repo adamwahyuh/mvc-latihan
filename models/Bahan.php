926732313bc8bda073f5e94c0c869b893e8642db
adamwahyuh <adamwahyu875@gmail.com>
2025-07-07 23:49:34 +0700

    Querying

--- models/Bahan.php
+++ models/Bahan.php
@@ -1,30 +1,52 @@
 <?php
 
-class Bahan {
-    private static $data = [
-        ['id' => 1, 'nama' => 'Kunyit',],
-        ['id' => 2, 'nama' => 'Jahe',],
-        ['id' => 3, 'nama' => 'Madu',],
-    ];
-    
-    public static function get() {
-        return self::$data;
-    }
-    public static function find($id) {
-        if(isset(self::$data)){
-            $hasil = array_filter(self::$data, function($item) use ($id) {
-                if($item['id'] == $id){
-                    return $item;
-                }
-                return false;
-            });
-            if (count($hasil)){
-                // $hasil = array_values($hasil); 
-                return $hasil[$id - 1];
-            }
-            return null;
-        }
-        return null;
+class Bahan
+{
+    private static function connect()
+    {
+        $db = new PDO("sqlite:" . __DIR__ . "/../database/database.db");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
     }
 
+    public static function get()
+    {
+        $db = self::connect();
+        $stmt = $db->query("SELECT * FROM bahan");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function find($id)
+    {
+        $db = self::connect();
+        $stmt = $db->prepare("SELECT * FROM bahan WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public static function create($data)
+    {
+        $db = self::connect();
+        $stmt = $db->prepare(" INSERT INTO bahan (nama, deskripsi, harga, jenis, foto) VALUES (:nama, :deskripsi, :harga, :jenis, :foto)");
+        $stmt->execute($data);
+        $data['id'] = $db->lastInsertId();
+        return $data;
+    }
+
+    public static function update($id, $data)
+    {
+        $db = self::connect();
+        $data['id'] = $id;
+        $stmt = $db->prepare(" UPDATE bahan SET nama = :nama, deskripsi = :deskripsi, harga = :harga, jenis = :jenis, foto = :foto WHERE id = :id");
+        $stmt->execute($data);
+        return self::find($id);
+    }
+
+    public static function delete($id)
+    {
+        $db = self::connect();
+        $stmt = $db->prepare("DELETE FROM bahan WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->rowCount() > 0;
+    }
 }
